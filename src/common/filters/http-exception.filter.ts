ce0e50c7abad5ec5a0862fd149f1f8f2037c4979
Soha Ali <sohaali710@gmail.com>
2025-09-02 22:28:29 +0300

    Update : Edit http exception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -5,25 +5,25 @@ import {
   HttpException,
 } from '@nestjs/common';
 
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = exception.getStatus();
+    // const message = exception ? exception.getResponse()['message'] : exception;
+    const exceptionResponse = exception.getResponse();
+    const error =
+      typeof exceptionResponse === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as Object);
 
-  @Catch(HttpException)
-  export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse();
-      const request = ctx.getRequest();
-      const status = exception.getStatus();
-      const message =
-      exception instanceof HttpException
-        ? exception.getResponse()['message']
-        : exception;
-  
-      response.status(status).json({
-        statusCode: status,
-        message,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
-    }
+    response.status(status).json({
+      ...error,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
   }
-  
+}
