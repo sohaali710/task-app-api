d30c13acef80df606f50dc4078592a1ae4c93622
Soha Ali <sohaali710@gmail.com>
2025-09-02 22:33:21 +0300

    Update : Implement configuration and validate it with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,25 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { MongoModule } from './mongo/mongo.module';
+import { MongoModule } from './database/mongo.module';
 import { TaskModule } from './task/task.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+      expandVariables: true, // if .env has VAR1=abc and VAR2=xyzabc so VAR2 can = xyz${VAR1}
+
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().required().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        DB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+      }),
     }),
 
     MongoModule,
@@ -20,7 +31,7 @@ import { TaskModule } from './task/task.module';
   controllers: [AppController],
   providers: [
     AppService,
-    { provide: 'APP_INTERCEPTOR', useClass: ClassSerializerInterceptor },
+    // { provide: 'APP_INTERCEPTOR', useClass: ClassSerializerInterceptor }, // to use globally, but the interceptor has a dependency (constructor)
   ],
 })
 export class AppModule {}
